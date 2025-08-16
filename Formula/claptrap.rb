bfef1531f5e81e2718e3346dbe44ed6239d5fd67
FujiApple <fujiapple852@gmail.com>
2025-08-16 18:15:12 +0800

    update fujiapple852/claptrap to 0.3.0

--- Formula/claptrap.rb
+++ Formula/claptrap.rb
@@ -1,9 +1,9 @@
 class Claptrap < Formula
   desc "Bring the power of Clap to shell scripts"
   homepage "https://claptrap.sh"
-  url "https://github.com/fujiapple852/claptrap/archive/refs/tags/0.2.0.tar.gz"
-  sha256 "1b0d938756985e307a467964cb82cf6933f6dcb61d86b3f535e6001056965b8e"
-  version "0.2.0"
+  url "https://github.com/fujiapple852/claptrap/archive/refs/tags/0.3.0.tar.gz"
+  sha256 "dbde95639da15e113c7372847c77a5969231772452333b688b3bae665f140b77"
+  version "0.3.0"
   license "Apache-2.0"
 
   depends_on "rust" => :build
